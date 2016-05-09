6216f35c9cad2598a1b38a707aaf5cc315144609
AminWatad <awata001@ucr.edu>
2016-05-09 15:26:01 -0700

    Add files via upload

--- menu.h
+++ menu.h
@@ -13,26 +13,63 @@ class Menu {
 
 	public:
 		Menu() {
-			//Base constructor to set up all necessary members
+			history_index = 0;
+			history.resize(0);
 		};
 		void execute() {
+			if(history.size() == 0)
+			{
+				cout << "0" << endl;
+				return;
+			}
+			cout << history.at(history_index)->execute() << endl;
 			//Prints the current commands value (based on history_index), if no commands exist
 			//print 0 by default
 		};
 		bool initialized() {
-			//Return true if the history has been primed with a single op instruciton
+			if(history.size() > 1)
+			{
+				return true;
+			}
+			return false;
+			//Return true if the history has been primed with a single op instruction
 			//This is necessary because that is the base of a calculation
 		};
 		void add_command(Command* cmd) {
+			if(history.at(history_index + 1)->execute() == cmd->execute())
+			{
+				++history_index;
+				return;
+			}
+			unsigned popbacks = history.size() - history_index;
+			for(unsigned i = 1; i < popbacks; ++i)
+			{
+				history.pop_back();
+			}
+			history.push_back(cmd);
+			if(history_index == 0)
+			{
+				return;
+			}
+			++history_index;
 			//Adds a command to history in the appropriate posiiton (based on history_index)
 		};
 		Command* get_command() {
+			return history.at(history_index);
 			//Returns the command in history we are currently referring to (based on history_index)
 		};
 		void undo() {
+			if(history_index != 0)
+			{
+				--history_index;
+			}
 			//Moves to the last command in history (if possible)
 		};
 		void redo() {
+			if(history_index != (history.size() - 1))
+			{
+				++history_index;
+			}
 			//Moves to the next command in history (if possible)
 		};
 };
